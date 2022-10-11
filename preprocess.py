eb0660a4cf15fba7d38ab9f20710b29710fd88ff
JeonJaeHyeong <wogud405@kaist.ac.kr>
2022-10-12 03:28:21 +0900

    change tokenizing option, add TextRank

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import load_data as ld
 import numpy as np
 import pandas as pd
 from eunjeon import Mecab
+from konlpy.tag import Komoran
 from soynlp.normalizer import *
 from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.feature_extraction.text import CountVectorizer
@@ -12,13 +13,8 @@ def clean_text(str):
     txt = only_text(txt)
     return txt
 
-def get_nouns(str):   # 형용사 등도 이용할 수 있도록
-    tagger = Mecab()
-    nouns_list = tagger.nouns(str)
-    return [ noun for noun in nouns_list if len(noun) != 1 ]
-
-def get_stcs_nouns(ListOfSentence):
-    return list(map(get_nouns, ListOfSentence))
+def get_stcs_tokens(ListOfSentence):
+    return list(map(mecab_tokenize, ListOfSentence))
 
 def make_dic_count(word_list):
     word_counts = dict()
@@ -36,28 +32,38 @@ def para2stcs(paragraph):
 def lst2str(lst):
     return ' '.join(lst)
 
-def cal_tfidf(nouns_paras):  # TF-IDF뿐만 아니라 textrank 등도 이용할 수 있도록
-    paragraphs = list(map(lst2str, nouns_paras))
+def mecab_tokenize(sent):
+    mecab = Mecab()
+    words = mecab.pos(sent)
+    words = [w[0] for w in words if ('NNP' in w[1] or 'XR' in w[1] or 'VA' in w[1] or 'NNG' in w[1])]
+    # XR : 어근, NNP : 고유명사, NNG : 보통명사, VA : 형용사
+    return words
+
+
+def cal_tfidf(tokens_paras):  # TF-IDF뿐만 아니라 textrank 등도 이용할 수 있도록
+    paragraphs = list(map(lst2str, tokens_paras))
     vectorizer = TfidfVectorizer()
     dtm = vectorizer.fit_transform(paragraphs)  # Document Term Matrix
 
     return dtm
 
 
-def preprocessing(paragraphs):  # paragraphs : ["문단1", "문단2", ...]
+def preprocess_node(paragraphs):  # paragraphs : ["문단1", "문단2", ...]
     
     combine_para = ' '.join(paragraphs)
     
     clean_comb = clean_text(combine_para)
+    tokens_comb = mecab_tokenize(clean_comb)
+
+    return tokens_comb #dic_comb, dic_paras, dic_stcs
+
+def preprocess_edge(paragraphs):  # paragraphs : ["문단1", "문단2", ...]
+    
     clean_paras = list(map(clean_text, paragraphs))     # [ [문단1], [문단2], ...]
     clean_stcs = list(map(para2stcs, clean_paras))      # [ [문단1의 문장1, 문단1의 문장2, ...], [문단2의 문장1, ...] , ...]
 
-    nouns_comb = get_nouns(clean_comb)
-    nouns_paras = list(map(get_nouns, clean_paras))     # [ [문단1의 명사들], [문단2의 명사들], ... ]
-    nouns_stcs = list(map(get_stcs_nouns, clean_stcs))  # [ [ [문단1의 문장1의 명사들], [문단1의 문장2의 명사들], ...], []]
+    tokens_paras = list(map(mecab_tokenize, clean_paras))     # [ [문단1의 명사들], [문단2의 명사들], ... ]
+    tokens_stcs = list(map(get_stcs_tokens, clean_stcs))  # [ [ [문단1의 문장1의 명사들], [문단1의 문장2의 명사들], ...], []]
 
-    dic_comb = make_dic_count(nouns_comb)
-    dic_paras = list(map(make_dic_count, nouns_paras))
-    dic_stcs = list(map(get_stcs_dic, nouns_stcs))
-    return nouns_comb, nouns_paras, nouns_stcs #dic_comb, dic_paras, dic_stcs
+    return tokens_paras, tokens_stcs #dic_comb, dic_paras, dic_stcs
     
